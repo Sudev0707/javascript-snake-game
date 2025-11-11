1b8b18be873c69e54feaeebf754cdaa718bce387
sudev <58890958+Sudev0707@users.noreply.github.com>
2025-11-12 04:23:16 +0530

    'change'

--- script.js
+++ script.js
@@ -105,7 +105,16 @@ startButton.addEventListener("click", () => {
   }, 300);
 });
 
-function restartGame() {}
+restartButton.addEventListener("click", restartGame);
+
+function restartGame() {
+  modal.style.display = "none";
+  snake = [{ x: 1, y: 3 }];
+  food = {
+    x: Math.floor(Math.random() * rows),
+    y: Math.floor(Math.random() * cols),
+  };
+}
 
 addEventListener("keydown", (event) => {
   console.log(event.key);
